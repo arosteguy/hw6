a540729f6db89e4443af8852937a0f84d82fdb2d
cake <larosteguy@gmail.com>
2019-12-30 18:17:27 -0700

    added local storage

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var APIKey = "0411c80deb16f2578650b7d2f0908cf3";
 var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=denver&appid=${APIKey}&units=imperial`;
-var city = $(this).attr(".city");
+// var city = $(this).attr(".city");
 var fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=denver&appid=${APIKey}&units=imperial`;
+var searchInput = document.querySelector("#search-text");
+var searchForm = document.querySelector("#search-form");
+var searchList = document.querySelector("#search-list");
+
+var saveCity = [];
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
-    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    $(".city").html("<h2>" + response.name + " Weather Details</h2>");
            $(".wind").text("Wind Speed: " + response.wind.speed);
             $(".humidity").text("Humidity: " + response.main.humidity);
            $(".temp").text("Temperature (F):  " + response.main.temp);
@@ -29,3 +34,61 @@ $.ajax({
         $$(".icon").img("" + response.list.weather.icon);
 console.log(fiveDayURL);
 });
+
+init();
+
+function renderSaveCity() {
+  searchList.innerHTML = "";
+
+    for (var i = 0; i < saveCity.length; i++) {
+    var city = saveCity[i];
+
+    var li = document.createElement("li");
+    li.textContent = city;
+    li.setAttribute("data-index", i);
+
+    searchList.appendChild(li);
+  }
+}
+
+function init() {
+  var storedSaveCity = JSON.parse(localStorage.getItem("saveCity"));
+
+  if (storedSaveCity !== null) {
+    saveCity = storedSaveCity;
+  }
+  renderSaveCity();
+}
+
+function storeSaveCity() {
+  
+  localStorage.setItem("saveCity", JSON.stringify(saveCity));
+}
+
+
+searchForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var inputText = searchInput.value.trim();
+
+  
+  if (inputText === "") {
+    return;
+  }
+
+    saveCity.push(inputText);
+    searchInput.value = "";
+
+  
+    storeSaveCity();
+    renderSaveCity();
+});
+
+    storeSaveCity();
+    renderSaveCity();
+
+    var a = moment.locale("America, Denver");
+
+    
+    var c = moment().format("LL");
+             document.getElementById("#dateDisplay").innerHTML = c;
