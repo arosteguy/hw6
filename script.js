d0c0939f5f12f7665522cdefd9c5cdbd489b72c7
cake <larosteguy@gmail.com>
2020-01-05 18:11:07 -0700

    renaming ids and adding functions

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
+var searchInput = $("#search-text");
 var APIKey = "0411c80deb16f2578650b7d2f0908cf3";
-var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=denver&appid=${APIKey}&units=imperial`;
-// var city = $(this).attr(".city");
-var fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=denver,us&appid=${APIKey}&units=imperial`;
-var searchInput = document.querySelector("#search-text");
-var searchForm = document.querySelector("#search-form");
-var searchList = document.querySelector("#search-list");
+var queryURL
+var fiveDayURL 
 
+
+var searchList = document.querySelector("#search-list");
+var searchButton = document.querySelector("#search-button");
 var saveCity = [];
 
 
-
+function showInfo(){
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
-    $(".city").html("<h2>" + response.name + "Weather Details </h2>");
-           $(".wind").text("Wind Speed: " + response.wind.speed);
-            $(".humidity").text("Humidity: " + response.main.humidity);
+    $(".city").html("<h2>" + response.name +  date + "  Weather Details  </h2>" );
+           $(".wind").text("Wind Speed: " + response.wind.speed + "MPH");
+           $(".humidity").text("Humidity: " + response.main.humidity + "%");
            $(".temp").text("Temperature (F):  " + response.main.temp);
 });
-
-$("#search").on("click", function() {
-    event.preventDefault();
-      $("#search").val().trim();
-    //this.response (city);
-    //console.log(this);
+}
+$(searchButton).on("click", function(event) {
+  event.preventDefault();
+  var userCity = $(searchInput).val().trim();
+  queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${APIKey}&units=imperial`;
+  fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userCity},us&appid=${APIKey}&units=imperial`;
+  saveCity.push(userCity);
+  console.log(fiveDayURL)
+  
+  console.log(saveCity)
+    showInfo();
+    showFiveDay();
 });
-    
+
+function showFiveDay(){
 $.ajax({
     url: fiveDayURL,
     method: "GET"
 }).then(function(response) {
-    $(".temperature").text("Temp: " + response.list.main.temp);
-        $(".humid").text("Humidity: " + response.list.main.humidity);
-        $$(".icon").img("" + response.list.weather.icon);
-console.log(fiveDayURL);
+  console.log(response)
+  for(var i = 5; i< 40; i+8 ){
+    $(".temperature").text("Temp: " + response.list[i].main.temp);
+    $(".humid").text("Humidity: " + response.list[i].main.humidity + "%");
+    $(".icon").img("" + response.list[i].main.weather.icon);
+    
+  }
+showFiveDay();
 });
+}
 
 init();
 
@@ -45,11 +57,11 @@ function renderSaveCity() {
     for (var i = 0; i < saveCity.length; i++) {
     var city = saveCity[i];
 
-    var li = document.createElement("li");
-    li.textContent = city;
-    li.setAttribute("data-index", i);
+    var button = document.createElement("button");
+    button.textContent = city;
+    button.setAttribute("data-index", i);
 
-    searchList.appendChild(li);
+    searchList.appendChild(button);
   }
 }
 
@@ -63,32 +75,33 @@ function init() {
 }
 
 function storeSaveCity() {
-  
   localStorage.setItem("saveCity", JSON.stringify(saveCity));
 }
-
-
-searchForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  var inputText = searchInput.value.trim();
-
   
+searchButton.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var inputText = searchInput.value.trim();
   if (inputText === "") {
     return;
   }
 
-    saveCity.push(inputText);
-    searchInput.value = "";
+  saveCity.push(inputText);
+  searchInput.value = "";
+  storeSaveCity();
+  renderSaveCity();
+
+});
+  storeSaveCity();
+  renderSaveCity();
+
 
   
-    storeSaveCity();
-    renderSaveCity();
-});
 
-    storeSaveCity();
-    renderSaveCity();
 
-    var a = moment.locale("America, Denver");
-    var c = moment().format("LL");
-    document.getElementById("#dateDisplay").innerHTML = c;
+  
+
+
+   var m = moment();
+    var date = m.format("L");
+    console.log(date);
+    
